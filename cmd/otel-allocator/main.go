6150ae5331d73bffcec6f5cc0e24e13fad7546a0
Grace Wehner <grace.wehner@microsoft.com>
2023-08-01 02:11:39 -0700

    eula statement

--- cmd/otel-allocator/main.go
+++ cmd/otel-allocator/main.go
@@ -64,6 +64,13 @@ func main() {
 		interrupts      = make(chan os.Signal, 1)
 		errChan         = make(chan error)
 	)
+
+	// EULA statement is required for Arc extension
+	clusterResourceId := os.Getenv("CLUSTER")
+	if strings.EqualFold(clusterResourceId, "connectedclusters") {
+		setupLog.Info("MICROSOFT SOFTWARE LICENSE TERMS\n\nMICROSOFT Azure Arc-enabled Kubernetes\n\nThis software is licensed to you as part of your or your company's subscription license for Microsoft Azure Services. You may only use the software with Microsoft Azure Services and subject to the terms and conditions of the agreement under which you obtained Microsoft Azure Services. If you do not have an active subscription license for Microsoft Azure Services, you may not use the software. Microsoft Azure Legal Information: https://azure.microsoft.com/en-us/support/legal/")
+	}
+
 	cliConf, err := config.ParseCLI()
 	if err != nil {
 		setupLog.Error(err, "Failed to parse parameters")
